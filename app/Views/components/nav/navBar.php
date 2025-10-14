b7fd079c08616e873ea462de11730bb36bbdcf2f
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2025-10-13 21:19:47 -0400

    subindo arquitetura do projeto

---
+++ app/Views/components/nav/navBar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+
+        <div>
+            <img src="" alt="Logo">
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Animais</li>
+            <li>Contato</li>
+            <li>Login</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+header('Location: ./../app/Views/pages/user/home.php');
+exit;
