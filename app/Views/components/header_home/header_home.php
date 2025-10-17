adef9aa3e23ea75c4da0034662195ba623f5deb9
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2025-10-16 21:22:36 -0400

    subindo

--- app/Views/components/header_home/header_home.php
+++ app/Views/components/header_home/header_home.php
@@ -1,9 +1,9 @@
     <header id="header_home">
         <section id="top">
             <div id="esquerda">
-                <img src="" alt="" id="1-esquerda">
-                <img src="" alt="" id="2-esquerda">
-                <img src="" alt="" id="3-esquerda">
+                <img src="./../../assets/img/passaro.png" alt="" id="topesquerda">
+                <img src="./../../assets/img/hammster.png" alt="" id="meioesquerda">
+                <img src="./../../assets/img/dalmata.png" alt="" id="botesquerda">
             </div>
             <div id="meio">
                 <img src="./../../assets/img/conectaPet.png" alt="Conecta Pet">
@@ -23,12 +23,22 @@
                 </div>
             </div>
             <div id="direita">
-                <div></div>
-                <div></div>
-                <div></div>
+                <img src="./../../assets/img/coelho.png" alt="" id="topdireita">
+                <img src="./../../assets/img/gato.png" alt="" id="meiodireita">
+                <img src="./../../assets/img/cachorro.png" alt="" id="botdireita">
             </div>
         </section>
-        <section id="bot">
-            wave
-        </section>
+
+        <div id="wave">
+            <svg id="topf" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#F3AA0D" fill-opacity="1"
+                    d="M0,224L48,234.7C96,245,192,267,288,250.7C384,235,480,181,576,138.7C672,96,768,64,864,90.7C960,117,1056,203,1152,250.7C1248,299,1344,309,1392,314.7L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
+                </path>
+            </svg>
+            <svg id="bot" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#FFD471" fill-opacity="1"
+                    d="M0,224L48,234.7C96,245,192,267,288,250.7C384,235,480,181,576,138.7C672,96,768,64,864,90.7C960,117,1056,203,1152,250.7C1248,299,1344,309,1392,314.7L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
+                </path>
+            </svg>
+        </div>
     </header>
