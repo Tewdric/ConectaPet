0f5bc7520163e59674c73476f0f1684c269e2fc3
Manoel V. L. de Oliveira <140553447+Tewdric@users.noreply.github.com>
2026-04-13 15:12:22 +0000

    Subindo correções de rota e estrutura de pagina

---
+++ app/Views/pages/adm/home_adm.php
@@ -0,0 +1,16 @@
+<?php
+    $titulo = 'Home';
+    include './../../components/head/head.php';
+?>
+
+<body>
+    <?php
+    include './../../components/nav/nav_adm/sideBar.php';
+    ?>
+
+    <div class="main-content">
+        <!-- seu conteúdo -->
+        <h1>aaa</h1>
+    </div>
+
+</body>
